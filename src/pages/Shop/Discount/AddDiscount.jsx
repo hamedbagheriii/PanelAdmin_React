05eb06222fc4c715ab2a4aeabc37760b4156ec58
hamed bagheri <hamedbagheri1386.19@gmail.com>
2024-09-01 20:10:29 +0330

    PART145-addDiscount(END(Post,edit,delete,get))

--- src/pages/Shop/Discount/AddDiscount.jsx
+++ src/pages/Shop/Discount/AddDiscount.jsx
@@ -166,6 +166,7 @@ const AddDiscount = () => {
                                         </div>
                                 
 
+
                                         <div className="modal-footer w-100 d-flex justify-content-around" >
                                             <button type="button" className="btn btn-danger modal-btn w-25"
                                             onClick={()=>navigate(-1)} data-bs-dismiss="modal">انصراف</button>
